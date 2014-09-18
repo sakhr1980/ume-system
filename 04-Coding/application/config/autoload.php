155ab1a142ab939903bcd71124ab25681ca93b70
sochy.choeun <sochy.choeun@6dfc1b4f-17a9-beac-2be4-b2f5d3e86c75>
2014-09-18 18:00:08 +0000

    Update task management

--- 04-Coding/application/config/autoload.php
+++ 04-Coding/application/config/autoload.php
@@ -67,7 +67,7 @@ $autoload['libraries'] = array('database', 'session', 'form_validation');
   |	$autoload['helper'] = array('url', 'file');
  */
 
-$autoload['helper'] = array('url', 'html', 'form', 'messages', 'pagination_config', 'general', 'csv','users');
+$autoload['helper'] = array('url', 'html', 'form', 'messages', 'pagination_config', 'general', 'csv');
 
 
 /*
--- 04-Coding/application/modules/users/views/functions/index.php
+++ 04-Coding/application/modules/users/views/functions/index.php
@@ -1,8 +1,8 @@
 <div class="toolbar col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
     <div class="left">
         <!--For icon: http://getbootstrap.com/components/-->
-        <a href="<?php echo site_url(); ?>users/modules/add/<?php echo $this->uri->segment(4); ?>" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-plus-sign"></i> Create Module</a>
-        <a href="<?php echo site_url(); ?>users/controller/add/<?php echo $this->uri->segment(4); ?>" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-plus-sign"></i> Create Controller</a>
+        <a href="<?php echo site_url(); ?>users/module/add/<?php echo $this->uri->segment(4); ?>" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-plus-sign"></i> Create Module</a>
+        <a href="<?php echo site_url(); ?>users/controllers/add/<?php echo $this->uri->segment(4); ?>" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-plus-sign"></i> Create Controller</a>
         <a href="<?php echo site_url(); ?>users/functions/add/<?php echo $this->uri->segment(4); ?>" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-plus-sign"></i> Create Function</a>
     </div>
     <div class="right">
